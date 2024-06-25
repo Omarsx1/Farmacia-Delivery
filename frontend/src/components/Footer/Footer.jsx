84a4a19fb14744870a18b09704ddab3f12354a03
Omar Salazar <salazarjuniorf@gmail.com>
2024-06-25 17:49:12 -0500

    Actualiza footer y header img

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,41 +1,54 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className='footer' id='footer'>
+    <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-            <img width={250} src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+          <img width={250} src={assets.logo} alt="Logo" />
+          <h3>Farmacia La Unión</h3>
+          <p>Rápido, confiable y siempre a tu alcance.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
         </div>
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
+          <h2>Menu</h2>
+          <ul>
+            <li>Inicio</li>
+            <li>Orden</li>
+            <li>Acerca de</li>
+            <li>Contacto</li>
+          </ul>
         </div>
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
-            </ul>
+          <h2>Hablemos</h2>
+          <ul>
+            <li><h3>Celular</h3></li>
+            <li>926 457 894</li>
+            <li>934 468 752</li>
+            <li><h3>Correo</h3></li>
+            <li>ventas@launionfarmacia.com</li>
+          </ul>
+        </div>
+        <div className="footer-content-address">
+          <h2>Dirección</h2>
+          <ul>
+            <li>Av. Alfredo Benavides 4652, Santiago de Surco 15038</li>
+          </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">
+        Copyright {currentYear} © farmacialaunion.com
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

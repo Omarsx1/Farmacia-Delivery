f31eee50cc15ef21a7f68059b8b88e21c9d571b1
Omar Salazar <salazarjuniorf@gmail.com>
2024-06-25 18:11:24 -0500

    instala dependencia fontawesome, actualiza footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Footer.css";
 import { assets } from "../../assets/assets";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope as faEnvelopeRegular } from '@fortawesome/free-regular-svg-icons';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -14,33 +16,33 @@ const Footer = () => {
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="Facebook" />
             <img src={assets.twitter_icon} alt="Twitter" />
-            <img src={assets.linkedin_icon} alt="LinkedIn" />
+            <FontAwesomeIcon icon={faEnvelopeRegular} className="social-icon" />
           </div>
         </div>
-        <div className="footer-content-center">
-          <h2>Menu</h2>
-          <ul>
-            <li>Inicio</li>
-            <li>Orden</li>
-            <li>Acerca de</li>
-            <li>Contacto</li>
-          </ul>
-        </div>
-        <div className="footer-content-right">
-          <h2>Hablemos</h2>
-          <ul>
-            <li><h3>Celular</h3></li>
-            <li>926 457 894</li>
-            <li>934 468 752</li>
-            <li><h3>Correo</h3></li>
-            <li>ventas@launionfarmacia.com</li>
-          </ul>
-        </div>
-        <div className="footer-content-address">
-          <h2>Dirección</h2>
-          <ul>
-            <li>Av. Alfredo Benavides 4652, Santiago de Surco 15038</li>
-          </ul>
+        <div className="footer-content-group">
+          <div className="footer-content-center">
+            <h2>Menu</h2>
+            <ul>
+              <li>Inicio</li>
+              <li>Orden</li>
+              <li>Acerca de</li>
+              <li>Contacto</li>
+            </ul>
+          </div>
+          <div className="footer-content-right">
+            <h2>Hablemos</h2>
+            <ul>
+              <li><h3>Celular</h3></li>
+              <li>926 457 894</li>
+              <li>934 468 752</li>              
+            </ul>
+          </div>
+          <div className="footer-content-address">
+            <h2>Dirección</h2>
+            <ul>
+              <li>Av. Alfredo Benavides 4652,<br /> Santiago de Surco 15038</li>
+            </ul>
+          </div>
         </div>
       </div>
       <hr />

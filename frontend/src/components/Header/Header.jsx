56f9c510b1e68b936afd50d4d222509990b123c4
Omarsx1 <salazarjuniorf@gmail.com>
2024-07-09 01:15:57 -0500

    Actualiza estilos Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
-        <h2 className="text-blue-600">Entrega a domicilio de productos farmacéuticos esenciales.</h2>
-        <p className="text-blue-300">Rápido, confiable y siempre a tu alcance.</p>
+        <h2 >Entrega a domicilio de productos farmacéuticos esenciales.</h2>
+        <p>Rápido, confiable y siempre a tu alcance.</p>
         <button className="bg-blue-600 px-4 py-2 rounded-full text-lg text-white">
           Ver Productos
         </button>
